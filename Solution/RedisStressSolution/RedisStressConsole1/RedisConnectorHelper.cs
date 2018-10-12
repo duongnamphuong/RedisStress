1af461ec91948689b66758c51455afb388939370
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2018-10-12 10:47:16 +0700

    Solution; project; StackExchange.Redis 1.2.6; RedisConnectorHelper; App.config: <add key="redisserver" value="localhost:6379" />

---
+++ Solution/RedisStressSolution/RedisStressConsole1/RedisConnectorHelper.cs
@@ -0,0 +1,27 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Configuration;
+
+namespace RedisConnectionTest
+{
+    internal class RedisConnectorHelper
+    {
+        static RedisConnectorHelper()
+        {
+            RedisConnectorHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect(ConfigurationManager.AppSettings["redisserver"]);
+            });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
