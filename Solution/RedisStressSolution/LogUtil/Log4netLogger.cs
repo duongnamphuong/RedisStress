38a91e43b8251122314a12949c34140779b0e9d8
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2018-10-19 11:49:57 +0700

    Add LogUtil.

---
+++ Solution/RedisStressSolution/LogUtil/Log4netLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUtil
+{
+    public static class Log4netLogger
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static void Info(object message)
+        {
+            log.Info(message);
+        }
+        public static void Info(object message, Exception exception)
+        {
+            log.Info(message, exception);
+        }
+    }
+}
