d43f515f123a3867351d66e40586dd2266bb98dc
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2018-10-19 10:28:44 +0700

    Data inserting script: 100000 Products, whose IMEI nvarchar(20) from N'00000000000000000001' to N'00000000000000100000'

---
+++ Database/Product Data.sql
@@ -0,0 +1,9 @@
+declare @Max int=100000
+declare @i int=1
+while @i<=@Max
+begin
+	--leading zeros, length of 20 characters
+	declare @Imei nvarchar(20) = RIGHT('00000000000000000000'+CAST(@i AS VARCHAR(20)),20)
+	insert into Product (Imei,[State],LastHbUtc) values(@Imei,1,null)
+	set @i=@i+1
+end
