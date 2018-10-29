193bc0ea987955443b6ee60ebb0784306b2dcbbc
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2018-10-29 13:56:20 +0700

    StatusFeeder: add QuartzJobs/HeartbeatFeeder.cs

---
+++ Solution/RedisStressSolution/StatusFeeder/QuartzJobs/HeartbeatFeeder.cs
@@ -0,0 +1,15 @@
+﻿using Quartz;
+using System;
+using System.Reflection;
+
+namespace StatusFeeder.QuartzJobs
+{
+    [DisallowConcurrentExecution]
+    internal class HeartbeatFeeder : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            LogUtil.Log4netLogger.Info(MethodBase.GetCurrentMethod().DeclaringType, $"{nameof(HeartbeatFeeder)}: {DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff")} UTC");
+        }
+    }
+}
