b92675e4ad9cddb722e82d0608b9b16fbe6970d5
Duong Nam Phuong <duongnamphuonghcm93@gmail.com>
2018-10-29 11:11:26 +0700

    QuartzDB: TestJob1.cs; Cron job test.

---
+++ Solution/RedisStressSolution/QuartzDB/Jobs/TestJob1.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+using System;
+
+namespace QuartzDB.Jobs
+{
+    internal class TestJob1 : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"{nameof(TestJob1)}: {DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff")} UTC");
+        }
+    }
+}
--- Solution/RedisStressSolution/QuartzDB/Test.cs
+++ Solution/RedisStressSolution/QuartzDB/Test.cs
@@ -1,9 +1,34 @@
-﻿namespace QuartzDB
+﻿using Quartz;
+using QuartzDB.Jobs;
+using System;
+
+namespace QuartzDB
 {
     internal class Test
     {
         private static void Main(string[] args)
         {
+            try
+            {
+                QuartzManagement.InitializeScheduler();
+
+                //clear
+                QuartzManagement.Clear();
+
+                //Add a cron job
+                IJobDetail job1 = JobBuilder.Create<TestJob1>().WithIdentity("myJob1", "myGroup1").Build();
+                string cronString = "0,15,30,45 * * ? * * *";
+                ITrigger trigger1 = TriggerBuilder.Create().WithIdentity("myTrigger1", "myGroup1").WithCronSchedule(cronString, x => x.InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("UTC"))).Build();
+                IScheduler sch = QuartzManagement.GetScheduler();
+                sch.ScheduleJob(job1, trigger1);
+
+                //start jobs
+                QuartzManagement.StartJobs();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
